c7a75ccfca50d1a35cb0361315f86fdb6df6ff1d
Jasper Paul Krol <jasper.paul.krol@gmail.com>
2021-10-02 21:49:37 +0200

    reels fixed

--- CasinoApplication/src/slotmachines/machines/ReelDeck.java
+++ CasinoApplication/src/slotmachines/machines/ReelDeck.java
@@ -1,4 +1,41 @@
 package slotmachines.machines;
 
+import slotmachines.Symbol;
+
+import java.util.List;
+
 public class ReelDeck {
+    //Attributes
+    private List<Reel> reels;
+
+    //Constructor
+    public ReelDeck(List<Reel> reels) {
+        //compist relatie dus in de constructor van ReelDeck instantieren
+        this.reels = reels;
+    }
+
+    //Methodes
+    public void roll() {
+        for (Reel reel : reels) {
+            reel.roll();
+        }
+    }
+
+    private String render() {
+        var renderedReels = "";
+
+        for (Reel reel : reels) {
+            renderedReels = renderedReels + " " + reel.render();
+        }
+        return renderedReels;
+    }
+
+    public boolean allReelsHaveSymbol(Symbol symbol) {
+        for (Reel reel : reels) {
+            if (reel.getSymbol() != symbol) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
