eaa2770e6feaa14428fc292a6865fa4b4474be32
Jasper Paul Krol <jasper.paul.krol@gmail.com>
2021-10-07 09:18:47 +0200

    maven jcorlor add tryout

--- CasinoApplication/src/Casino.java
+++ CasinoApplication/src/Casino.java
@@ -1,22 +1,21 @@
 import novi.Game;
+import novi.Printer;
 import novi.blackjackgames.LowDeckBlackJack;
 import novi.blackjackgames.SimpleBlackJack;
 import novi.hangman.HangmanGame;
 import novi.higerlower.HigherLowerGame;
 import novi.slotmachines.SlotMachinesGame;
-
-import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Casino {
 
+public class Casino {
     private final Scanner inputScanner;
     private final List<Game> games;
     private int coins;
 
-    public Casino(Scanner inputScanner){
+    public Casino(Scanner inputScanner) {
         this.inputScanner = inputScanner;
         this.games = new ArrayList<>();
         this.games.add(new HigherLowerGame(inputScanner));
@@ -28,16 +27,16 @@ public class Casino {
         this.coins = 1000;
     }
 
-    public void start(){
-        System.out.printf("\n$$$ Welcome to the Golden Triangle casino! $$$\n");
+    public void start() {
+        Printer.printTitle("\n$$$ Welcome to casino the Golden Triangle! $$$\n");
 
-        while (true){
+        while (true) {
             System.out.println("Press p to start a game or q to quit");
             String input = inputScanner.next();
 
-            if (input.equalsIgnoreCase("p")){
-                playGame();
-            } else if (input.equalsIgnoreCase("q")){
+            if (input.equals("p")) {
+                playAGame();
+            } else if (input.equals("q")) {
                 System.out.printf("Thank you for playing, you ended with %d coins", coins);
                 inputScanner.close();
                 System.exit(0);
@@ -45,7 +44,7 @@ public class Casino {
         }
     }
 
-    public void playGame(){
+    private void playAGame() {
         System.out.printf("You have %d coins which allows you to play the following games.\n", coins);
 
         var playableGames = getPlayableGames();
@@ -54,43 +53,46 @@ public class Casino {
         }
 
         var currentGame = getGameToPlay(playableGames);
+
         currentGame.playGame(coins);
 
         var coinResult = currentGame.getWinnings();
         processWinnings(coinResult);
     }
 
-    private List<Game> getPlayableGames(){
+    private List<Game> getPlayableGames() {
         List<Game> filteredGames = new ArrayList<>();
 
         for (Game game : games) {
-            if (game.getMinimalRequiredCoins() <= this.coins){
+            if (game.getMinimalRequiredCoins() <= this.coins) {
                 filteredGames.add(game);
             }
         }
+
         return filteredGames;
     }
 
-    private Game getGameToPlay(List<Game> playableGames){
-        while(true){
-            System.out.printf("Which game do you want to play? (0 - %d)\n", playableGames.size()-1);
+    private Game getGameToPlay(List<Game> playableGames) {
+        while (true) {
+            System.out.printf("Which game do you want to play? (0 - %d)\n", playableGames.size() - 1);
 
-            if (inputScanner.hasNextInt()){
+            if (inputScanner.hasNextInt()) {
                 int index = inputScanner.nextInt();
-                if (index >= 0 && index <= playableGames.size()){
+                if (index >= 0 && index < playableGames.size()) {
                     return playableGames.get(index);
                 }
             }
         }
     }
 
-    private void processWinnings(int winnings){
+    private void processWinnings(int winnings) {
         this.coins = this.coins + winnings;
+
         if (winnings >= 0) {
-            System.out.printf("You won %d coins\n", winnings);
+            Printer.printGreen("You won %d coins", winnings);
         } else {
-            System.out.printf("You lost %d coins\n", -winnings);
+            Printer.printRed("You lost %d coins", -winnings);
         }
-        System.out.printf("You now have %d coins\n", coins);
+        Printer.printGreen("You now have %d coins\n", coins);
     }
 }
